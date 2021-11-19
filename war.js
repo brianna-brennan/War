72dd0227396a3937945168eaf4adefd7304bc178
brianna brennan <brianna.brennanj@gmail.com>
2021-11-19 14:28:03 -0800

    adds index.html and war.js

---
+++ war.js
@@ -0,0 +1,23 @@
+//1. Define required constants:
+//1.1 Define deck of cards with values that indicate what theyre worth 
+//1.2 Define two players, player 1 and 2 as objects each with total score and current card 
+
+
+//2. Upon loading the app should:
+//Button and playing field should load
+// Render a message: 
+//If statement: If player 1 choice is equal to player 2 choice display "tie" message, if player 1 choice
+//is greater than player 2 choice or vice versa display which player wins 
+
+
+//3. Drawing cards:
+//Use an event listener to randomly select a card when the player
+//clicks "draw card" button
+//use math.random to randomly select a card from the cards array
+//loop through if statements to determine winner based on card values 
+//if returned true, declare winner
+
+
+
+
+
